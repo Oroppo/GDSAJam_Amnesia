68fbbe05b0cebb47e191a1ffcd588e2bc6978794
rcfieldhouse <rcfieldhouse@yahoo.com>
2023-10-21 01:34:00 -0400

    goat things

--- GDSAJam/Assets/Goat.cs
+++ GDSAJam/Assets/Goat.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goat : MonoBehaviour
+public class Goat : FallingItem
 {
+    public LayerMask WhatIsJumpable;
+    public float GoatJumpRange = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,16 @@ public class Goat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var colls = Physics2D.OverlapCircleAll(GetComponent<Rigidbody2D>().position, GoatJumpRange, WhatIsJumpable);
+        foreach(Collider2D coll in colls)
+            Debug.Log(coll.gameObject.name);
+    }
+    public override void ItemDeath()
+    {
+        Destroy(gameObject);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, GoatJumpRange);
     }
 }

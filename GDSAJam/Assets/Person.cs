4b272e355bd0096d9caab8e2bcc9f942d45fd38a
rcfieldhouse <rcfieldhouse@yahoo.com>
2023-10-21 23:35:06 -0400

    game is playable(ish)

--- GDSAJam/Assets/Person.cs
+++ GDSAJam/Assets/Person.cs
@@ -33,8 +33,8 @@ public class Person : FallingItem
         {
             yield return new WaitForSeconds(SinkInterval);
             Duration += SinkInterval;
-            coll.size = new Vector2(0.29f*(1f - Duration / SinkTime), 1);
-            coll.offset = new Vector2(0.29f * (1f - coll.size.x) / 2f, 0);
+            coll.size = new Vector2(0.44f * (1f - Duration / SinkTime), 1);
+            coll.offset = new Vector2(0.44f * (1f - coll.size.x) / 2f-0.04f, 0);
         }
         ItemDeath();
     }
